e6df337115a65735f4bc51433cbbc8027e72ea77
Nicolas2931 <nrubios@udistrital.edu.co>
2025-02-06 21:13:21 -0500

    hola2

---
+++ src/app/components/anualidades/anualidades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anualidades',
+  templateUrl: './anualidades.component.html',
+  styleUrl: './anualidades.component.css'
+})
+export class AnualidadesComponent {
+
+}
