4e9ea077388a058b2b5110dcfd2c92a63af7f6bc
jjleon22 <leonjairetsell@gmail.com>
2025-02-06 22:36:51 -0500

    Tabla

--- src/app/components/amortizacion/amortizacion.component.ts
+++ src/app/components/amortizacion/amortizacion.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './amortizacion.component.css'
 })
 export class AmortizacionComponent {
+  rows: number = 0;
+  cols: number = 0;
+  table: any[][] = [];
 
+  generateTable() {
+    this.table = [];
+    for (let i = 0; i < this.rows; i++) {
+      this.table[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.table[i][j] = `Row ${i + 1}, Col ${j + 1}`;
+      }
+    }
+  }
 }
