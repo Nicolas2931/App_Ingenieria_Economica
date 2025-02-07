4e95029ffb50e7626621a927e81cf1fc43af3fcb
Nicolas2931 <nrubios@udistrital.edu.co>
2025-02-06 22:03:29 -0500

    Pedir i o j

--- src/app/components/anualidades/anualidades.component.ts
+++ src/app/components/anualidades/anualidades.component.ts
@@ -8,10 +8,19 @@ import { Component } from '@angular/core';
 export class AnualidadesComponent {
     S:Number;
     A:Number;
-    i:Number;
+    i?: number;
+    j?: number;
     n:String;
+    get deshabilitarJ(): boolean {
+      return this.i !== undefined && this.i !== null;
+    }
+
+    get deshabilitarI(): boolean {
+      return this.j !== undefined && this.j !== null;
+    }
+
 
     calcularAnualidad(){
-      
+
     }
 }
