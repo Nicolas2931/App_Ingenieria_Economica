023ad7643e39a700f422398a8b9fda67d5703bae
Jean Paul Toloza <135926488+jptolozac@users.noreply.github.com>
2025-02-07 02:40:13 +0000

    add: conversion service

--- src/app/services/conversion-tasas.service.ts
+++ src/app/services/conversion-tasas.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Periodos } from './periodos.enum';
 
+interface convertir_tasa_props {
+  i?: number;
+  m: Periodos;
+  j?: number;
+  n: Periodos;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,22 +15,36 @@ export class ConversionTasasService {
 
   constructor() { }
 
+  nominal_efectiva(j: number, m: Periodos) {
+    return j / m;
+  }
   
-  efectiva_nominal(i: Number|null,n_i: Periodos, j:Number,n_j:Periodos):number{
-    let m:Number;
-    if(n_i===n_j){
-      if(i===null && j!=null){
-        m = 12/n_j;
-        return j/m;
-      }
-      else{
-        //return j=i*m;
-      }
-    }
-    else{
-
-    }
-
+  efectiva_nominal(i: number, m:Periodos): number{
+    return i * m;
   }
 
+  conversion_tasas(i: number, n: Periodos, m: Periodos) {
+    return ((1+i)^(n/m)) - 1;
+  }
+
+  convertir_a_nominal({ i, n, m }: convertir_tasa_props){
+    i = this.conversion_tasas(i,n,m)
+
+    return this.efectiva_nominal(i,m)
+  }
+
+  convertir_a_efectiva({ j, n, m }: convertir_tasa_props){
+    const i = this.nominal_efectiva(j,m)
+    const conversion = this.conversion_tasas(i,n,m)
+    console.log("conversion", conversion)
+    return 
+  }
+
+  convertir_tasa({ i, j, n, m }: convertir_tasa_props) {
+    if(i && !j) {
+      return this.convertir_a_nominal({i,n,m})
+    } 
+    
+    return this.convertir_a_efectiva({ j, n, m })
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Periodos } from './app/services/periodos.enum';
+import { ConversionTasasService } from './app/services/conversion-tasas.service';
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+
+const conversion = new ConversionTasasService()
+
+const j = 0.32;
+const m = Periodos.TRIMESTRE
+// const i = conversion.nominal_efectiva(j,m)
+const n = Periodos.MENSUAL
+
+const i = conversion.convertir_tasa({ j, m, n })
+console.log(i)
