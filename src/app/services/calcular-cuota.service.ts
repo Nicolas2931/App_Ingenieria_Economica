2a4c2d2dc9f7ddc528032b560b5ec83929aa7c93
Nicolas2931 <nrubios@udistrital.edu.co>
2025-02-07 22:42:17 -0500

    Anualidades

---
+++ src/app/services/calcular-cuota.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalcularCuotaService {
+
+  constructor() { }
+
+  calcular_A_S(S:number, i:number,n:number):number{
+    let A:number;
+    A=(S*i)/(Math.pow(1 +i,n) - 1) ;
+    return A;
+  }
+
+  calcular_A_P(P:number, i:number,n:number):number{
+    let A:number;
+    A=P/((1-Math.pow(1 +i,-n)) / i) ;
+    return A;
+  }
+
+  calcular_S(A:number, i:number,n:number):number{
+    let S:number;
+    S=A*((Math.pow(1 +i,n) - 1) / i) ;
+    return S;
+  }
+
+  calcular_P(A:number, i:number,n:number):number{
+    let P:number;
+    P=A*((1-Math.pow(1 +i,-n)) / i) ;
+    return P;
+  }
+
+}
